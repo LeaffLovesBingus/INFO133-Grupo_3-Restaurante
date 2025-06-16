a413c7ff304a7406ab248646588ef100cc03fe9e
Matias <m.toledovarela@gmail.com>
2025-06-16 11:20:16 -0400

    interfaz casi final

--- interfaz_transaccional/test_medio_pago.py
+++ interfaz_transaccional/test_medio_pago.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
+from medio_pago import create as create_medio_pago, update as update_medio_pago, delete as delete_medio_pago
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
+    return render_template(template_name_or_list='index.html')
+
+@app.route('/medio_pago/<string:action>')
+
+def medio_pago(action):
+
     # Connect to the database
     conn = psycopg2.connect(
-    host="localhost",
-    database="sistema_restaurante",
-    user="usuario_restaurante",
-    password="1234"
+        host="localhost",
+        database="sistema_restaurante",
+        user="usuario_restaurante",
+        password="1234"
     )
 
     # create a cursor
@@ -27,75 +34,20 @@ def index():
     cur.close()
     conn.close()
 
-    return render_template('medio_pago.html', data=data)
+    return render_template(template_name_or_list='medio_pago.html', data=data, action=action)
 
-@app.route('/create', methods=['POST'])
+@app.route('/medio_pago/create', methods=['POST'])
+def create_medio_pago_route():
+    return create_medio_pago()
 
-def create():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="sistema_restaurante",
-        user="usuario_restaurante",
-        password="1234"
-    )
-    cur = conn.cursor()
-    name = request.form['name']
-    cur.execute(
-        '''INSERT INTO "Medio_Pago" ("Medio_Pago") VALUES (%s)''',
-        (name,)
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('medio_pago'))
+@app.route('/medio_pago/update', methods=['POST'])
+def update_medio_pago_route():
+    return update_medio_pago()
 
-@app.route('/update', methods=['POST'])
+@app.route('/medio_pago/delete', methods=['POST'])
+def delete_medio_pago_route():
+    return delete_medio_pago()
 
-def update():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="sistema_restaurante",
-        user="usuario_restaurante",
-        password="1234"
-    )
-    cur = conn.cursor()
-    name = request.form['name']
-    id = request.form['id']
-    cur.execute(
-        '''UPDATE "Medio_Pago" SET "Medio_Pago"=%s WHERE "Id_Medio_Pago"=%s''',
-        (name, id)
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('medio_pago'))
-
-@app.route('/delete', methods=['POST'])
-
-def delete():
-    conn = psycopg2.connect(
-    host="localhost",
-    database="sistema_restaurante",
-    user="usuario_restaurante",
-    password="1234"
-    )
-
-    cur = conn.cursor()
-
-    # Get the data from the form
-    id = request.form['id']
-
-    # Delete the data from the table
-    cur.execute('''DELETE FROM "Medio_Pago" WHERE "Id_Medio_Pago"=%s''', (id,))
-
-    # commit the changes
-    conn.commit()
-
-    # close the cursor and connection
-    cur.close()
-    conn.close()
-
-    return redirect(url_for('medio_pago'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
